4b3dce098255472e5cfcf96705014e44e889c0be
Женя Минюк <minyk20000@gmail.com>
2021-11-07 17:20:04 +0300

    Добавьте файлы проекта.

---
+++ PhotoViewer/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhotoViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Открыть изображение";
+            ofd.Filter = ".jpg files(*.jpg)|*.jpg|.png files(*.png)|*.png|All files(*.*)|*.*";
+            if (DialogResult.OK == ofd.ShowDialog())
+            {
+                pictureBox1.Image = Image.FromFile(ofd.FileName);
+            }
+        }
+    }
+}
